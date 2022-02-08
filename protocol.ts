faf427380758310b1826dd089139a2090bff7ddf
Catalin Vajaiala <catalin.vajaiala@gmail.com>
2022-02-09 00:58:14 +0200

    Add communication with server using Axios

--- protocol.ts
+++ protocol.ts
@@ -1,4 +1,10 @@
-type Message = {
-    uid: string;
-    message: string;
+export type Message = {
+    uid: String;
+    message: String;
+}
+
+export type SerialMessage = {
+    uid: String;
+    length: number;
+    message: String;
 }
